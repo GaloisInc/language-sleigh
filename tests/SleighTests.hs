26bef7bf3292cf69b3597a518c6d0f8705f6185a
Tristan Ravitch <tristan@ravit.ch>
2022-07-18 22:06:09 -0700

    Add integration tests (#1)
    This adds ghidra as a submodule to make the Sleigh specs easily accessible

--- tests/SleighTests.hs
+++ tests/SleighTests.hs
@@ -3,6 +3,8 @@ module Main ( main ) where
 
 import qualified Data.ByteString.Char8 as BSC
 import qualified Data.Foldable as F
+import qualified Prettyprinter as PP
+import           System.FilePath ( (</>) )
 import qualified Test.Tasty as TT
 import qualified Test.Tasty.HUnit as TTH
 import qualified Text.Megaparsec as TM
@@ -21,6 +23,7 @@ toPreprocessorTest (str, expected) = TTH.testCase str $ do
                     ]
       TTH.assertEqual "Tokens" expected actuals
 
+-- | Manually-written preprocessor tests, verifying that the expected tokens are generated
 preprocessorTests :: TT.TestTree
 preprocessorTests = TT.testGroup "Preprocessor Tests" (map toPreprocessorTest expected)
   where
@@ -36,9 +39,52 @@ preprocessorTests = TT.testGroup "Preprocessor Tests" (map toPreprocessorTest ex
                , ("f-", [LS.FSub])
                ]
 
+mipsDir :: FilePath
+mipsDir = "ghidra/Ghidra/Processors/MIPS/data/languages"
+
+mipsTargets :: [String]
+mipsTargets = [ "mips32le.slaspec"
+              , "mips32be.slaspec"
+              ]
+
+ghidraPreprocessorTest
+  :: FilePath
+  -- ^ The include path (and directory containing the target)
+  -> String
+  -- ^ The target file to preprocess
+  -> TT.TestTree
+ghidraPreprocessorTest dir target = TTH.testCase ("Preprocess:" ++ name) $ do
+  let includePath = dir
+  bytes <- BSC.readFile name
+  eres <- LS.preprocessSleigh includePath name bytes
+  case eres of
+    Left err -> TTH.assertFailure (TM.errorBundlePretty err)
+    Right _tokens -> return ()
+  where
+    name = dir </> target
+
+ghidraParserTest
+  :: FilePath
+  -- ^ The include path (and directory containing the target)
+  -> String
+  -- ^ The target file to parse
+  -> TT.TestTree
+ghidraParserTest dir target = TTH.testCase ("Parse:" ++ name) $ do
+  let includePath = dir
+  bytes <- BSC.readFile name
+  eres <- LS.parseSleigh includePath name bytes
+  case eres of
+    Left err -> TTH.assertFailure (show (PP.pretty err))
+    Right _ast -> return ()
+  where
+    name = dir </> target
+
 allTests :: TT.TestTree
 allTests =
-  TT.testGroup "All Tests" [ preprocessorTests ]
+  TT.testGroup "All Tests" (concat [ [preprocessorTests]
+                                   , fmap (ghidraPreprocessorTest mipsDir) mipsTargets
+                                   , fmap (ghidraParserTest mipsDir) mipsTargets
+                                   ])
 
 main :: IO ()
 main = TT.defaultMain allTests
